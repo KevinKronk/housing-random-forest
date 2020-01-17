a94f0a3c3b6684b1389936f0222ca200f99bc17d
Kevin Kronk <kjkronk@ncsu.edu>
2020-01-17 16:46:36 -0500

    Project Code

---
+++ housing-random-forest/full_pipeline.py
@@ -0,0 +1,237 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import hashlib
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from pandas.plotting import scatter_matrix
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import FeatureUnion
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+
+filename = "housing.csv"
+
+
+def load_housing_data(filename):
+    housing = pd.read_csv(filename)
+    return housing
+
+
+housing = load_housing_data(filename)
+
+# print(housing.info())
+# print(housing['ocean_proximity'].value_counts())
+# print(housing.describe())
+
+
+# create new category for stratified sampling
+# divide by 1.5 to get a better distribution, ceiling for definitive categories
+housing["income_cat"] = np.ceil(housing['median_income'] / 1.5)
+housing['income_cat'].where(housing['income_cat'] < 5, 5.0, inplace=True)
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+# print(len(train_set), len(test_set))
+
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing['income_cat']):
+    strat_train_set = housing.iloc[train_index]
+    strat_test_set = housing.iloc[test_index]
+
+# print(len(strat_train_set), len(strat_test_set))
+# print(housing['income_cat'].value_counts() / len(housing))
+
+# print(train_set['income_cat'].value_counts() / len(train_set))
+# print(strat_train_set['income_cat'].value_counts() / len(strat_train_set))
+
+# remove 'income_cat' so data is back to its original state
+
+strat_train_set = strat_train_set.drop(columns=['income_cat'])
+strat_test_set = strat_test_set.drop(columns=['income_cat'])
+
+
+# strat_train_set.hist(bins=50, figsize=(20, 15))
+# plt.show()
+
+# Make copy to explore it without harming training set
+housing = strat_train_set.copy()
+
+# Separate the predictors and the target values (drop creates a copy)
+housing = strat_train_set.drop('median_house_value', axis=1)
+housing_labels = strat_train_set['median_house_value'].copy()
+
+
+# Custom Transformers
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
+
+
+class MyLabelBinarizer(TransformerMixin):
+    def __init__(self, *args, **kwargs):
+        self.encoder = LabelBinarizer(*args, **kwargs)
+
+    def fit(self, x, y=0):
+        self.encoder.fit(x)
+        return self
+
+    def transform(self, x, y=0):
+        return self.encoder.transform(x)
+
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True):  # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+
+    def fit(self, X, y=None):
+        return self  # nothing else to do
+
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+        population_per_household = X[:, population_ix] / X[:, household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.attribute_names].values
+
+
+housing_num = housing.drop('ocean_proximity', axis=1)
+
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+num_pipeline = Pipeline([
+    ('selector', DataFrameSelector(num_attribs)),
+    ('imputer', SimpleImputer(strategy='median')),
+    ('attribs_adder', CombinedAttributesAdder()),
+    ('std_scaler', StandardScaler()),
+])
+
+cat_pipeline = Pipeline([
+    ('selector', DataFrameSelector(cat_attribs)),
+    ('label_binarizer', MyLabelBinarizer()),
+])
+
+full_pipeline = FeatureUnion(transformer_list=[
+    ('num_pipeline', num_pipeline),
+    ('cat_pipeline', cat_pipeline),
+])
+
+housing_prepared = full_pipeline.fit_transform(housing)
+print(housing_prepared, housing_prepared.shape)
+
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+
+some_data = housing.iloc[:5]
+some_labels = housing_labels.iloc[:5]
+some_data_prepared = full_pipeline.transform(some_data)
+print('Predictions:', lin_reg.predict(some_data_prepared))
+print('Labels:', list(some_labels))
+
+# Linear Regression
+housing_predictions = lin_reg.predict(housing_prepared)
+lin_mse = mean_squared_error(housing_labels, housing_predictions)
+lin_rmse = np.sqrt(lin_mse)
+print(lin_rmse)  # This is an example of a model underfitting
+
+# Decision Tree
+tree_reg = DecisionTreeRegressor()
+tree_reg.fit(housing_prepared, housing_labels)
+housing_predictions = tree_reg.predict(housing_prepared)
+tree_mse = mean_squared_error(housing_labels, housing_predictions)
+tree_rmse = np.sqrt(tree_mse)
+print(tree_rmse)
+
+scores = cross_val_score(tree_reg, housing_prepared, housing_labels,
+                         scoring='neg_mean_squared_error', cv=10)
+tree_rmse_scores = np.sqrt(-scores)
+print("Before Display")
+
+
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Std:", scores.std())
+
+
+# Decision Tree
+display_scores(tree_rmse_scores)
+
+scores = cross_val_score(lin_reg, housing_prepared, housing_labels,
+                         scoring='neg_mean_squared_error', cv=10)
+lin_rmse_scores = np.sqrt(-scores)
+display_scores(lin_rmse_scores)
+print("Made it here")
+
+# Random Forest
+forest_reg = RandomForestRegressor()
+print("Before Here")
+forest_reg.fit(housing_prepared, housing_labels)
+print("Here")
+scores = cross_val_score(forest_reg, housing_prepared, housing_labels,
+                         scoring='neg_mean_squared_error', cv=10)
+forest_rmse_scores = np.sqrt(-scores)
+display_scores(forest_rmse_scores)
+print("Made it here too")
+
+# Grid Search Random Forest
+param_grid = [
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+]
+print("Bout to do grid search")
+forest_reg = RandomForestRegressor()
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5, scoring='neg_mean_squared_error')
+grid_search.fit(housing_prepared, housing_labels)
+print(grid_search.best_params_)
+print(grid_search.best_estimator_)
+
+cvres = grid_search.cv_results_
+for mean_score, params in zip(cvres['mean_test_score'], cvres['params']):
+    print(np.sqrt(-mean_score), params)
+
+# Analyze model and its error
+feature_importances = grid_search.best_estimator_.feature_importances_
+print(feature_importances)
+# Add in feature names
+extra_attribs = ['rooms_per_hhold', 'pop_per_hhold', 'bedrooms_per_room']
+# cat_one_hot_attribs = list(encoder.classes_)
+attributes = num_attribs + extra_attribs
+print(sorted(zip(feature_importances, attributes), reverse=True))
+
+final_model = grid_search.best_estimator_
+
+x_test = strat_test_set.drop('median_house_value', axis=1)
+y_test = strat_test_set['median_house_value'].copy()
+
+x_test_prepared = full_pipeline.transform(x_test)
+final_predictions = final_model.predict(x_test_prepared)
+
+final_mse = mean_squared_error(y_test, final_predictions)
+final_rmse = np.sqrt(final_mse)
+
+print(final_rmse)
+
+
